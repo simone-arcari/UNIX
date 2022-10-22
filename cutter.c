b4f37ec62f4a8739dee610fd2c1278fe66d43737
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2022-10-22 20:11:57 +0200

    Add files via upload

---
+++ cutter.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_PARAM   2
+#define MAX_LEN     1024
+#define CHIOCCIOLA  '@'
+
+int main(int argc, char** argv) {
+    char buffer[MAX_LEN];
+    char* filename;
+    char* output_filename = "cutted.txt";
+    FILE* file;
+    FILE* output;
+    int len;
+
+    if(argc != NUM_PARAM) {
+        printf("error number of param\n");
+        printf("usage: ./name filename\n");
+        exit(EXIT_FAILURE);
+    
+    }
+
+    filename = argv[1];
+
+
+    file = fopen(filename, "r+");
+    output = fopen(output_filename, "w+");
+    if(file == NULL || output == NULL) {
+        perror("error fopen()");
+        exit(EXIT_FAILURE);
+
+    }
+
+    while(fscanf(file, "%s", buffer ) != EOF) {
+
+        len = strlen(buffer);
+        for(int i=0; i<len; i++) {
+            if(buffer[i] == CHIOCCIOLA){
+                fprintf(output, "%s\n", buffer);
+                fflush(output);
+                break;
+            }
+        }
+    }
+    
+
+
+    return EXIT_SUCCESS;
+
+}
